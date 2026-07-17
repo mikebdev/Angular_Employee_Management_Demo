dd7071240abf02fb425fd6b7ef11c366d7b9a661
Michael Belcher <mbdev@outlook.com>
2026-07-17 13:32:29 -0400

    LoginStuff

--- src/app/pages/header/header.ts
+++ src/app/pages/header/header.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+
+  isCollapsed = false;
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+
+  }
+
+}
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -10,6 +12,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class Login {
   http = inject(HttpClient);
+  router = inject(Router);
   hostAddress: string = "https://localhost:7004"
 
   // API endpoints
@@ -17,7 +20,12 @@ export class Login {
   getAllDepartmentsURL: string = this.hostAddress + '/api/DepartmentMaster/GetAllDepartments';
 
   // will have JWT token authentication "Header, Payload, Signature", but for now, we will just have a simple login form
-  // case needs to match what api expects, so we will use "Email" and "Password" instead of "email" and "password"
+  // Payload can use postman or other client and the login form to test this.
+  // {
+  // "email": "someemail@outlook.com",
+  // "password": "thepassword"
+  // }
+  
   loginObj: any = {
     Email: '',
     Password: ''
@@ -30,10 +38,17 @@ export class Login {
       next: (result: any) => {
         console.log(result);
         // Handle successful login, e.g., store token, redirect, etc.
+        // For demonstration, we will just log the result and show an alert
+        // alert('Login successful! Token: ' + result.token);
+        //alert(result.message);
+        this.router.navigate(['/dashboard']); // Redirect to dashboard or another page after successful login
+        //LocalStorage.setItem('token', result.token); // JWT token can be stored in local storage for future requests, but for now, we will just store the user data in local storage for testing purposes
+        localStorage.setItem('empLoginUser', JSON.stringify(result.data)); // Store the user data in local storage for testing, session storage can also be used, but local storage is more persistent across sessions
       },
       error: (error: any) => {
         console.error('Login failed:', error);
         // Handle login error, e.g., show error message to user
+        alert('Login failed. Please check your credentials and try again.' + error.message);
       }
     })
   }
